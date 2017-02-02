95cc0f48f1d1967166c2785fb5b0622538688d30
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-02-03 02:46:08 +1100

    Adding N Queens backtracking solution

---
+++ Backtracking/Main.cpp
@@ -0,0 +1,28 @@
+#include "Queens.h"
+#include <cstdio>
+#include <ctime>
+
+void test_get_total() {
+    algorithms::backtracking::Queens queen;
+    for (int n = 1; n <= 15; ++n) {
+        std::clock_t start = clock();
+        printf("------------------------------------------------\n");
+        printf("N == [%d] || Total == [%d]\n", n, queen.Total(n));
+        printf("Execution time     == [%.8f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
+        printf("------------------------------------------------\n");       
+    }
+}
+
+void test_draw_all(int n) {
+    algorithms::backtracking::Queens queen;
+    std::clock_t start = clock();
+    queen.ComputeAll(n);
+    printf("Execution time     == [%.8f] seconds\n", (clock() - start) / static_cast<double>(CLOCKS_PER_SEC));
+}
+
+int main() {
+    test_get_total();
+    test_draw_all(8);
+    
+    return 0;
+}
---
+++ Backtracking/Queens.h
@@ -0,0 +1,39 @@
+/*
+  --------------------------------------------------------------------------------------
+  				     THE QUEENS PROBLEM
+  --------------------------------------------------------------------------------------
+  A well known problem is to count the number of different ways in which eight
+  queens can be placed on an 8×8 chess board without any two of them sharing the
+  same row, column or diagonal.
+  --------------------------------------------------------------------------------------
+
+  Algorithm based on Backtracking using Bit Patterns and Recursion
+
+  Author: Rakesh Kumar, cpp.rakesh@gmail.com
+  Date  : Feb 2nd, 2017
+ */
+
+#include <string>
+
+namespace algorithms {
+    namespace backtracking {
+        class Queens {
+        public:
+            Queens();
+            ~Queens();
+
+            int Total(int n);
+            void ComputeAll(int n);
+
+        private:
+            int m_all;
+            int m_total_count;
+            std::string m_row_positions;
+            
+            void m_total(int ld, int col, int rd);
+            bool m_is_valid(int queen_number, int pos) const;
+            void m_compute(int k);
+            void m_draw_board();
+        };
+    };
+};
